891a30cfe4f5f977c440af9619c8140ab23571a8
Elton Tang <etang315@gmail.com>
2018-12-25 00:39:38 -0600

    Edit the for loop by changing c to Array.length to provide more accurate and consistent counter.

--- eclipse-workspace/Hackerrank_Solving_Problem/src/Simply_Array_Sum.java
+++ eclipse-workspace/Hackerrank_Solving_Problem/src/Simply_Array_Sum.java
@@ -7,11 +7,15 @@ public class Simply_Array_Sum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] Array;
+		
+		//Declaring the size of the array.
 		System.out.println("The amount of number you wish to enter?");
 		int c = sc.nextInt();
 		Array = new int[c];
 		int sum = 0;
-		for(int i=0;i<c;i++) {
+		
+		//This for loop take in number to the array and add the total of each number to declare variable sum.
+		for(int i=0;i<Array.length;i++) {
 			
 			System.out.println("Enter a number:");
 			Array[i] = sc.nextInt();
